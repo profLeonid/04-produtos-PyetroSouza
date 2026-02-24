1847f3df1d71bda18cb95de96100a97e572730bf
PYETRO FERREIRA DE SOUZA <25205137@dsn127m1246730.cfp127aluno.local>
2026-02-24 15:11:25 -0300

    feat: adicionar caixa de texto vazia após adicionar o produto

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 'use strict'
 
 function adicionarProduto(){
+    if (produto.value = ""){
+        alert("Coloca o produto mano")
+    }
     const produto = document.getElementById("produto")
     const lista = document.getElementById("lista")
 
@@ -9,4 +12,5 @@ function adicionarProduto(){
     span.className = 'bg-blue-200 px-8 py-2 '
 
     lista.appendChild(span)
+    produto.value = ""
 }
