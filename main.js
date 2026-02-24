68ea83b498c21408f72db70f627d2686a806181b
PYETRO FERREIRA DE SOUZA <25205137@dsn127m1246730.cfp127aluno.local>
2026-02-24 15:12:24 -0300

    feat: alertar caso adicionar o produto vazio

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 'use strict'
-
+   
 function adicionarProduto(){
-    if (produto.value = ""){
-        alert("Coloca o produto mano")
-    }
     const produto = document.getElementById("produto")
     const lista = document.getElementById("lista")
-
     const span = document.createElement('span')
+    if (produto.value == ``){
+        alert("Coloca o produto mano")
+    } else {
+
     span.textContent = produto.value
     span.className = 'bg-blue-200 px-8 py-2 '
-
     lista.appendChild(span)
     produto.value = ""
+    }
 }
